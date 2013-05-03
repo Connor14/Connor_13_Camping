3d107ae8a8cbcd6092ea53e78bc34101eb212066
Connor Schmidt <schmidty68connor@gmail.com>
2013-05-03 14:59:51 -0500

    FirePit Update

--- camping/common/blocks/CampingBlockFirePit.java
+++ camping/common/blocks/CampingBlockFirePit.java
@@ -2,6 +2,9 @@ package Connor_13_Camping.camping.common.blocks;
 
 import java.util.Random;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 import Connor_13_Camping.camping.common.CampingMod;
 import Connor_13_Camping.camping.common.tile_entities.CampingTileEntityFirePit;
 
@@ -10,6 +13,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.world.World;
 
 public class CampingBlockFirePit extends BlockContainer
@@ -17,49 +21,70 @@ public class CampingBlockFirePit extends BlockContainer
 	private Class TestEntityClass;
 
 	public CampingBlockFirePit(int i,Class tClass)
-		{
-		  super(i,Material.rock);
-		  TestEntityClass = tClass;
-	      float f = 0F;
-	      //Block Bounds
-	      setBlockBounds(0F, 0F, 0F, 1F, .25F, 1F);
-		}
+	{
+	  super(i,Material.rock);
+	  TestEntityClass = tClass;
+      float f = 0F;
+      setBlockBounds(0F, 0F, 0F, 1F, .25F, 1F);
+      this.setTickRandomly(true);
+	}
    
     public TileEntity getBlockEntity()
     {
-            try{
-                    return (TileEntity)TestEntityClass.newInstance();
-            }
-            catch(Exception exception){
-                    throw new RuntimeException(exception);
-            }
-    }
-    //Spawn Particle
-    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
-    {        
-    	int l = par1World.getBlockMetadata(par2, par3, par4);
-    	double d0 = (double)((float)par2 + 0.5F);
-    	double d1 = (double)((float)par3 + 0.7F);
-    	double d2 = (double)((float)par4 + 0.5F);
-    	double d3 = 0.2199999988079071D;
-    	double d4 = 0.27000001072883606D;
-    	
-    	if (par5EntityPlayer.getCurrentEquippedItem() != null && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.flintAndSteel.itemID)
+    	try
     	{
-            par1World.spawnParticle("smoke", d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
-            par1World.spawnParticle("flame", d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
-            return true;
+    		return (TileEntity)TestEntityClass.newInstance();
     	}
-    	if (par5EntityPlayer.getCurrentEquippedItem() != null && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.bucketWater.itemID)
+    	catch(Exception exception)
     	{
-    		return false;
+    		throw new RuntimeException(exception);
     	}
-    	else
-    	{
-    		return super.onBlockActivated(par1World, par2, par3, par4, par5EntityPlayer, par6, par7, par8, par9);
-    	}
-    }
+    } 
     
+    private boolean isActive = false;
+    
+    /**
+     * Called upon block activation (right click on the block.)
+     */
+    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+    {
+        if(par5EntityPlayer.getCurrentEquippedItem() != null)
+        {
+        	if(isActive && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.bucketWater.itemID)
+        	{
+        		isActive = false;
+        		return true;
+        	}
+        	else if (!isActive && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.flintAndSteel.itemID)
+        	{
+        		isActive = true;
+        		return true;
+        	}
+        }
+        
+        return super.onBlockActivated(par1World, par2, par3, par4, par5EntityPlayer, par6, par7, par8, par9);
+    }
+
+    @SideOnly(Side.CLIENT)
+    /**
+     * A randomly called display update to be able to add particles or other items for display
+     */
+    public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
+    {
+        if (isActive)
+        {
+            float f = (float)par2 + 0.5F;
+            float f1 = (float)par3 + 0.0F + par5Random.nextFloat() * 6.0F / 16.0F;
+            float f2 = (float)par4 + 0.5F;
+            float f3 = 0.52F;
+            float f4 = par5Random.nextFloat() * 0.6F - 0.3F;
+
+            par1World.spawnParticle("smoke", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", (double)(f - f3), (double)f1, (double)(f2 + f4), 0.0D, 0.0D, 0.0D);
+    
+        }
+    }
+
     public int idDropped(int i, Random random, int j)
     {
         return CampingMod.firepitIcon.itemID;

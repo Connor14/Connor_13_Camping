3f42347ebb2c777bc668292163427bd42fcbd4a2
Connor Schmidt <schmidty68connor@gmail.com>
2013-05-03 11:10:01 -0500

    Small Update

--- camping/common/blocks/CampingBlockFirePit.java
+++ camping/common/blocks/CampingBlockFirePit.java
@@ -36,11 +36,19 @@ public class CampingBlockFirePit extends BlockContainer
     }
     //Spawn Particle
     public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
-    {
+    {        
+    	int l = par1World.getBlockMetadata(par2, par3, par4);
+    	double d0 = (double)((float)par2 + 0.5F);
+    	double d1 = (double)((float)par3 + 0.7F);
+    	double d2 = (double)((float)par4 + 0.5F);
+    	double d3 = 0.2199999988079071D;
+    	double d4 = 0.27000001072883606D;
+    	
     	if (par5EntityPlayer.getCurrentEquippedItem() != null && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.flintAndSteel.itemID)
     	{
-    		this.randomDisplayTick(par1World, par2, par3, par4, null);
-    		return true;
+            par1World.spawnParticle("smoke", d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
+            par1World.spawnParticle("flame", d0 - d4, d1 + d3, d2, 0.0D, 0.0D, 0.0D);
+            return true;
     	}
     	if (par5EntityPlayer.getCurrentEquippedItem() != null && par5EntityPlayer.getCurrentEquippedItem().itemID == Item.bucketWater.itemID)
     	{
